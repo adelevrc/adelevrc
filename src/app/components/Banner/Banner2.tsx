aeb017056db69e168511691298096be30163aa0e
adelevrc <adele.vercaygne@gmail.com>
2024-09-24 07:54:59 +0200

    fix styles

--- src/app/components/Banner/Banner2.tsx
+++ src/app/components/Banner/Banner2.tsx
@@ -2,7 +2,7 @@ import styles from "./banner.module.scss";
 
 const Banner = () => {
   return (
-    <section className={`${styles.banner} ${styles.bannerWhite}`}>
+    <section className={`${styles.banner}`}>
       <h2>
         Trouvez votre <span> force </span>
         mentale et physique
