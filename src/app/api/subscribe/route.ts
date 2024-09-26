db27601bbbabf44f06c9bc786773f20439407d34
adelevrc <adele.vercaygne@gmail.com>
2024-09-26 15:45:08 +0200

    add subscription

---
+++ src/app/api/subscribe/route.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mailchimp from '@mailchimp/mailchimp_marketing';
+import { NextResponse } from 'next/server';
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY as string,
+  server: process.env.MAILCHIMP_SERVER_PREFIX as string,
+});
+
+type Data = {
+  message?: string;
+  error?: string;
+};
+
+export async function POST(
+  req: Request,
+  _: NextResponse<Data>
+) {
+  const email  = await req.json();
+   if (!email ) {
+    return NextResponse.json({error: 'Email is required',message: "400" });
+  }
+
+   try {
+    const listId = process.env.MAILCHIMP_AUDIENCE_ID as string;
+    await mailchimp.lists.addListMember(listId, {
+      email_address: email,
+      status: 'subscribed',
+    });
+
+    return NextResponse?.json({ message: '200' });
+  } catch (error: any) {
+    return NextResponse.json({
+      error: `Error subscribing to the newsletter: ${error.message}`,
+    });
+  } 
+}
