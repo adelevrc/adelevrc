bd0afe2b3713b70ef47e5f7586ad9841dfff5981
adelevrc <adele.vercaygne@gmail.com>
2024-12-04 16:23:24 +0100

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,20 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { Libre_Bodoni, Mrs_Saint_Delafield } from "next/font/google";
+
 import "./styles/globals.scss";
+
+export const libreBodoni = Libre_Bodoni({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+export const mrsSaintDelafield = Mrs_Saint_Delafield({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
