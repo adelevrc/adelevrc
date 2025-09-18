2cf347663e785d0f6c8ea3193c7b5d47c6b72ccf
adelevrc <adele.vercaygne@gmail.com>
2025-09-18 11:17:39 +0200

    fix layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -23,21 +23,21 @@ export default async function LocaleLayout({
   params: { locale: string };
 }) {
   const { locale } = await params;
-  /* 
+
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
   setRequestLocale(locale);
-  const messages = await getMessages(); */
+  const messages = await getMessages();
   return (
     <html lang='fr'>
       <body>
-        {/*     <NextIntlClientProvider messages={messages}>
-          <Header /> */}
-        {children}
-        {/*         <LanguageSwitcher />
-          <Footer />
-        </NextIntlClientProvider> */}
+        <NextIntlClientProvider messages={messages}>
+          {/*    <Header /> */}
+          {children}
+          {/*  <LanguageSwitcher />
+             <Footer />*/}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
