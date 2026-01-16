f7f5a8aa0958b317e4532d254510f03cb879b0d4
adelevrc <adele.vercaygne@gmail.com>
2026-01-16 12:51:17 +0100

    fix : url

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const createApolloClient = () => {
   return new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:3000/graphql", // URL du back
+    uri: process.env.NODE_BACK_URL, 
   }),    cache: new InMemoryCache(),
   });
 };
